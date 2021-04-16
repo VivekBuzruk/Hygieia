4fbd9118a6c8a9eed7799323afcea7dd11aec30a
VivekBuzruk <vivek_buzruk@yahoo.com>
2021-04-16 14:32:00 +0530

    Utils to create few collections

---
+++ utils/import_profile.sh
@@ -0,0 +1,3 @@
+mongoimport /db:dashboarddbZZZ /collection:profiles  --host localhost:27017 /authenticationDatabase:admin /username:XXX /password:YYY mcicd-gates.json
+
+mongoimport /db:dashboarddbZZZ /collection:maturity_model --host localhost:27017 /authenticationDatabase:admin /username:XXX /password:YYY mcicd-gates.json
